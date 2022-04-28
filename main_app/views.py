4c5270df30b817ff433c5a10a86f3a8f93616d04
philkatie <kaphillips219@gmail.com>
2022-04-28 18:22:09 -0500

    adding index views

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def index(request):
+    return HttpResponse('<h1>this is the index page</h1>')
