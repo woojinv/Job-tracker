28e611d38fb031dda71ccfcae442769a26bd60ee
amnonongus <charmilano@gmail.com>
2022-04-29 11:48:02 -0700

    added edit btn

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('jobs/', views.JobList.as_view(), name='jobs_index'),
     path('jobs/<int:pk>/', views.JobDetail.as_view(), name='jobs_detail'),
     path('jobs/create/', views.JobCreate.as_view(), name='jobs_create'),
+    path('jobs/<int:pk>/update/', views.JobUpdate.as_view(), name='jobs_update'),
 ]
