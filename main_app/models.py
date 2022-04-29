6f8df6af65386370174c92efa3d48df73d19cd75
amnonongus <charmilano@gmail.com>
2022-04-29 10:26:53 -0700

    updating job model

--- main_app/models.py
+++ main_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     company = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    title = models.CharField('Job Title', max_length=100)
     salary = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
-    date_applied = models.DateField('date applied')
-    tech_reqs = models.TextField(max_length=500)
+    date_applied = models.DateField('Date Applied')
+    tech_reqs = models.TextField('Technical Requirements', max_length=500)
     status = models.CharField(max_length=50)
     source = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
