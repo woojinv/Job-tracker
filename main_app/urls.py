16d3c318ce2bd1293a73e5d5f897671799259c5d
woojinv <oh.woojin95@gmail.com>
2022-04-29 15:30:21 -0500

    create signup View

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # Jobs
     path('jobs/', views.JobList.as_view(), name='jobs_index'),
     path('jobs/<int:pk>/', views.JobDetail.as_view(), name='jobs_detail'),
     path('jobs/create/', views.JobCreate.as_view(), name='jobs_create'),
     path('jobs/<int:pk>/update/', views.JobUpdate.as_view(), name='jobs_update'),
     path('jobs/<int:pk>/delete/', views.JobDelete.as_view(), name='jobs_delete'),
+    
+    # Users
+    path('accounts/sign/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Job
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'home.html')
@@ -28,3 +30,18 @@ class JobUpdate(UpdateView):
 class JobDelete(DeleteView):
     model = Job
     success_url = '/jobs/'
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('jobs_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
