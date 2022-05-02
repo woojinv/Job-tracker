139e27978b6a5aacffea5ac0d56daf7e49d79493
philkatie <kaphillips219@gmail.com>
2022-05-02 17:53:00 -0500

    playing with apis

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
 
     # Jobs
     path('jobs/', views.JobList.as_view(), name='jobs_index'),
-    path('jobs/<int:pk>/', views.JobDetail.as_view(), name='jobs_detail'),
+    # path('jobs/<int:pk>/', views.JobDetail.as_view(), name='jobs_detail'),
+    path('jobs/<int:job_id>', views.jobs_detail, name='jobs_detail'),
     path('jobs/create/', views.JobCreate.as_view(), name='jobs_create'),
     path('jobs/<int:pk>/update/', views.JobUpdate.as_view(), name='jobs_update'),
     path('jobs/<int:pk>/delete/', views.JobDelete.as_view(), name='jobs_delete'),
