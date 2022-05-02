4004bc5d5d18b87d748ede31a2e905e18284faee
woojinv <oh.woojin95@gmail.com>
2022-05-02 18:38:13 -0500

    set up environmental variables

--- main_app/views.py
+++ main_app/views.py
@@ -5,13 +5,17 @@ from .models import Job
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
 # import requests
 import clearbit
+
+
 
 def home(request):
 
     variable_name = 'twitch'
-    clearbit.key = 'sk_d07964faa899c455e891fd4c3a1bb102'
+    clearbit.key = settings.API_KEY
+
     company = clearbit.Company.find(domain=f'{variable_name}.com',stream=True)
 
     return render(request, 'home.html', {
@@ -31,7 +35,7 @@ class JobList(LoginRequiredMixin, ListView):
 
 def jobs_detail(request, job_id):
     job = Job.objects.get(id=job_id)
-    clearbit.key = 'sk_d07964faa899c455e891fd4c3a1bb102'
+    clearbit.key = settings.API_KEY
     company = clearbit.Company.find(domain=f'{job.company}.com',stream=True)
 
     return render(request, 'main_app/job_detail.html', {
