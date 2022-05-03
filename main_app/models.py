cdc8e3d1b09aae2c1133201e7dbc4271e058be63
amnonongus <charmilano@gmail.com>
2022-05-03 13:56:15 -0700

    create Document model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
+class Document(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 # Create your models here.
 class Job(models.Model):
     company = models.CharField(max_length=100)
@@ -14,6 +20,7 @@ class Job(models.Model):
     source = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    documents = models.ManyToManyField(Document)
 
     def __str__(self):
         return f"{self.company}"
