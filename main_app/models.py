06e464ff934048adcfa512744e90b8f6a4c113fe
woojinv <oh.woojin95@gmail.com>
2022-05-03 17:26:16 -0500

    update document model and create document view

--- main_app/models.py
+++ main_app/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-
-class Document(models.Model):
-    # name = models.CharField(max_length=100)
-    url = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"document for {self.user} at {self.url}."        
-
 # Create your models here.
 class Job(models.Model):
     company = models.CharField(max_length=100)
@@ -23,10 +14,21 @@ class Job(models.Model):
     source = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    documents = models.ManyToManyField(Document)
+    # documents = models.ManyToManyField(Document)
 
     def __str__(self):
         return f"{self.company}"
     
     def get_absolute_url(self):
         return reverse('jobs_detail', kwargs={'job_id': self.id})
+
+
+
+class Document(models.Model):
+    # name = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"document for {self.job_id} at {self.url}."  
