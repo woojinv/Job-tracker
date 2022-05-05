24e37a82a50abd0befa9e26a71faab5551be3b0c
philkatie <kaphillips219@gmail.com>
2022-05-05 15:47:23 -0500

    settings

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import environ
 
-env = environ.Env()
+environ.Env()
 environ.Env.read_env()
+
+env = environ.Env()
+# environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env['SECRET_KEY']
-API_KEY = env['API_KEY']
-BUCKET_NAME = env['BUCKET_NAME']
-S3_BASE_URL = env['S3_BASE_URL']
+SECRET_KEY = env('SECRET_KEY')
+API_KEY = env('API_KEY')
+BUCKET_NAME = env('BUCKET_NAME')
+S3_BASE_URL = env('S3_BASE_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='staticfiles'
 LOGIN_REDIRECT_URL = '/jobs/'
 LOGOUT_REDIRECT_URL = '/'
 
@@ -132,6 +136,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
